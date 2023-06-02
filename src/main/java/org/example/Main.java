c9c1cd6f3279d4e8e7742738d09b601b874ee988
Mathias Hahner <mathias_hahner@hotmail.com>
2023-06-02 19:21:37 -0300

    adding project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,74 @@
+package org.example;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    private static final int NUM_ELEMENTS = 1000;
+
+    public static void main(String[] args) {
+
+        int[] orderArray = getArray();
+        int[] reverseArray = getReverseArray();
+        int[] randomArray = getRandomArray();
+
+        int numberSwaps;
+        Instant startTime, finishTime;
+        QuickSort quickSort = new QuickSort();
+
+        startTime = Instant.now();
+        numberSwaps = quickSort.quickSort(orderArray, 0, orderArray.length - 1);
+        finishTime = Instant.now();
+        System.out.println("Order:   " + getElapsedTime(startTime, finishTime) + "ns");
+        System.out.println("Swaps: " + numberSwaps);
+
+        startTime = Instant.now();
+        numberSwaps = quickSort.quickSort(reverseArray, 0, reverseArray.length - 1);
+        finishTime = Instant.now();
+        System.out.println("Reverse: " + getElapsedTime(startTime, finishTime) + "ns");
+        System.out.println("Swaps: " + numberSwaps);
+
+        startTime = Instant.now();
+        numberSwaps = quickSort.quickSort(randomArray, 0, randomArray.length - 1);
+        finishTime = Instant.now();
+        System.out.println("Random:  " + getElapsedTime(startTime, finishTime) + "ns");
+        System.out.println("Swaps: " + numberSwaps);
+    }
+
+    private static double getElapsedTime(Instant startTime, Instant finishTime) {
+        return Duration.between(startTime, finishTime).toMillis();
+    }
+
+    private static int[] getArray() {
+        int[] arr = new int[NUM_ELEMENTS];
+
+        for (int i = 0; i < NUM_ELEMENTS; i++)
+            arr[i] = i;
+        return arr;
+    }
+
+    private static int[] getReverseArray() {
+        int[] arr = new int[NUM_ELEMENTS];
+
+        for (int i = NUM_ELEMENTS - 1; i > 0; i--)
+            arr[i - 1] = NUM_ELEMENTS - i;
+        return arr;
+    }
+
+    private static int[] getRandomArray() {
+        int[] arr = new int[NUM_ELEMENTS];
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < NUM_ELEMENTS; i++)
+            list.add(i);
+        Collections.shuffle(list);
+
+        for (int i = 0; i < NUM_ELEMENTS; i++)
+            arr[i] = list.get(i);
+        return arr;
+    }
+}
---
+++ src/main/java/org/example/QuickSort.java
@@ -0,0 +1,41 @@
+package org.example;
+
+public class QuickSort {
+
+    private int numberSwaps = 0;
+
+    public int quickSort(int[] arr, int begin, int end) {
+        quickSortAlgoritm(arr, begin, end);
+        return numberSwaps;
+    }
+
+    private void quickSortAlgoritm(int[] arr, int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(arr, begin, end);
+            quickSortAlgoritm(arr, begin, partitionIndex - 1);
+            quickSortAlgoritm(arr, partitionIndex + 1, end);
+        }
+    }
+
+    private int partition(int[] arr, int begin, int end) {
+        int pivot = arr[end];
+        int i = (begin - 1);
+
+        for (int j = begin; j < end; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+
+                int swapTemp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = swapTemp;
+            }
+        }
+
+        int swapTemp = arr[i + 1];
+        arr[i + 1] = arr[end];
+        arr[end] = swapTemp;
+        numberSwaps++;
+
+        return i + 1;
+    }
+}
